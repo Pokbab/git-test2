7e41551481bd542b8037e1ae64ea4656abb766d3
최강훈/고객센터개발팀 <kanghoon.choi@nhnent.com>
2015-05-04 15:21:49 +0900

    헬로월드

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+/*
+ * @(#)Test.java $version 2015. 5. 4.
+ *
+ * Copyright 2015 NHN Ent. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author Kanghoon Choi
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
